bbe795d2264e9cf6020c3e1150397f539355975f
syalosovetskyi <43996021+syalosovetskyi@users.noreply.github.com>
2021-08-17 22:05:22 +0300

    f

--- taskTest/run.py
+++ taskTest/run.py
@@ -5,6 +5,13 @@ from hypercorn.asyncio import serve
 from app import create_app
 app_ = create_app()
 
+bashCommand = "ip addr| grep eth0"
+import subprocess
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+print(output)
+print(output.split())
+print(output.split()[13])
 config = Config()
 # config.from_mapping(bind='http://0.0.0.0:5000')
 config.bind = ["172.17.62.242:80"] 
